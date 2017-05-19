3608cb7a9ed2288615b7e3c0cc7ebdac8c2ce3f2
Franz Gregor <franz.gregor@tu-dresden.de>
2017-05-19 09:54:16 +0000

    run_experiment.sh edited online with Bitbucket

--- run_experiment.sh
+++ run_experiment.sh
@@ -26,20 +26,20 @@ if [ ! -z "$IMAGE" ]; then
     # replace cds-tool in student image
     CONTAINER=$(docker run -v $(pwd)/cds-root/cds-tool/bin/:/mnt -d $USERNAME bash -c 'eval "cp /mnt/cds-tool `which cds-tool`"')
     docker wait ${CONTAINER}
-    # delete original image
-    docker rmi ${IMAGE}
+    ORIG_IMAGE=${IMAGE}
     # put modified image instead
-    IMAGE=`docker commit ${CONTAINER} ${USERNAME}:latest`
+    IMAGE=`docker commit ${CONTAINER} ${USERNAME}`
     docker rm ${CONTAINER}
+    # delete original image
+    docker rmi ${ORIG_IMAGE}
 fi
 
-
-if [ ! -z "$CONTAINER" ]; then
+if [ ! -z "$IMAGE" ]; then
     cd /data/cdslab/$USERNAME
-    ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/string-parsing/judge.in -o 11mopp/string-parsing/judge.out 11mopp-string-parsing > sp.$BUILD_TAG.log
-    ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/sudokount/judge.in -o 11mopp/sudokount/judge.out 11mopp-sudokount > sd.$BUILD_TAG.log
-    ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/histogram/judge.in -o 11mopp/histogram/judge.out 11mopp-histogram > hg.$BUILD_TAG.log
-    ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/game-of-life/judge.in -o 11mopp/game-of-life/judge.out 11mopp-game-of-life > gl.$BUILD_TAG.log
+    RUST_LOG=debug ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/string-parsing/judge.in -o 11mopp/string-parsing/judge.out 11mopp-string-parsing > sp.$BUILD_TAG.log
+    RUST_LOG=debug ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/sudokount/judge.in -o 11mopp/sudokount/judge.out 11mopp-sudokount > sd.$BUILD_TAG.log
+    RUST_LOG=debug ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/histogram/judge.in -o 11mopp/histogram/judge.out 11mopp-histogram > hg.$BUILD_TAG.log
+    RUST_LOG=debug ./cds-root/cds-tool/bin/cds-tool run --measure --image $USERNAME -c 1,2,4,8 --input 11mopp/game-of-life/judge.in -o 11mopp/game-of-life/judge.out 11mopp-game-of-life > gl.$BUILD_TAG.log
 fi
 
 CONTAINER=$( docker ps -a --no-trunc | grep '$USERNAME' | awk '{ print $1 }' | tr '\n' ' ')
