94f7cbca37852668007dd422e9c7fee7eaec0b68
Shermineh Shojaei Berjouei <sherminshojaei@gmail.com>
2017-07-01 17:13:33 +0200

    add it

--- 11mopp/game-of-life/life.c
+++ 11mopp/game-of-life/life.c
@@ -20,13 +20,17 @@ typedef unsigned char cell_t;
 cell_t ** allocate_board (int size) {
 	cell_t ** board = (cell_t **) malloc(sizeof(cell_t*)*size);
 	int	i;
+
 	for (i=0; i<size; i++)
 		board[i] = (cell_t *) malloc(sizeof(cell_t)*size);
 	return board;
+
+
 }
 
 void free_board (cell_t ** board, int size) {
         int     i;
+#pragma omp parallel for private(i)
         for (i=0; i<size; i++)
                 free(board[i]);
 	free(board);
@@ -41,30 +45,28 @@ int adjacent_to (cell_t ** board, int size, int i, int j) {
 	int ek = (i+1 < size) ? i+1 : i;
 	int sl = (j>0) ? j-1 : j;
         int el = (j+1 < size) ? j+1 : j;
-
-
+ 
+  #pragma omp parallel for private(k,l,i,j)
 	for (k=sk; k<=ek; k++)
 		for (l=sl; l<=el; l++)
 			count+=board[k][l];
-	count-=board[i][j];
-	
+	count-=board[i][j];	
 	return count;
+
 }
 
+
+
 void play (cell_t ** board, cell_t ** newboard, int size) {
+
 	int	i, j, a;
 	/* for each cell, apply the rules of Life */
 
-
-
-
 	for (i=0; i<size; i++)
-
- #pragma omp parallel for private(a)
-
-
+#pragma omp parallel private(a,j) 
+#pragma omp for schedule(guided)
 		for (j=0; j<size; j++) {
-                    
+               
 			a = adjacent_to (board, size, i, j);
 			if (a == 2) newboard[i][j] = board[i][j];
 			if (a == 3) newboard[i][j] = 1;
@@ -72,10 +74,7 @@ void play (cell_t ** board, cell_t ** newboard, int size) {
 			if (a > 3) newboard[i][j] = 0;
 		}
 
-
 }
-
-
 
 /* print the life board */
 void print (cell_t ** board, int size) {
@@ -83,6 +82,7 @@ void print (cell_t ** board, int size) {
 	/* for each row */
 	for (j=0; j<size; j++) {
 		/* print each column position... */
+
 		for (i=0; i<size; i++) 
 			printf ("%c", board[i][j] ? 'x' : ' ');
 		/* followed by a carriage return */
