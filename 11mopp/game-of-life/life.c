a4d20ce11cff6cf3a40379cbfb6669775c31f4c6
Shermineh Shojaei Berjouei <sherminshojaei@gmail.com>
2017-07-02 19:17:50 +0200

    add

--- 11mopp/game-of-life/life.c
+++ 11mopp/game-of-life/life.c
@@ -20,7 +20,7 @@ typedef unsigned char cell_t;
 cell_t ** allocate_board (int size) {
 	cell_t ** board = (cell_t **) malloc(sizeof(cell_t*)*size);
 	int	i;
-
+#pragma omp parallel for 
 	for (i=0; i<size; i++)
 		board[i] = (cell_t *) malloc(sizeof(cell_t)*size);
 	return board;
@@ -30,7 +30,7 @@ cell_t ** allocate_board (int size) {
 
 void free_board (cell_t ** board, int size) {
         int     i;
-#pragma omp parallel for private(i)
+#pragma omp parallel for
         for (i=0; i<size; i++)
                 free(board[i]);
 	free(board);
@@ -45,12 +45,14 @@ int adjacent_to (cell_t ** board, int size, int i, int j) {
 	int ek = (i+1 < size) ? i+1 : i;
 	int sl = (j>0) ? j-1 : j;
         int el = (j+1 < size) ? j+1 : j;
- 
-  #pragma omp parallel for private(k,l,i,j)
-	for (k=sk; k<=ek; k++)
+
+//#pragma omp parallel for private(k,l,i,j) 
+	for (k=sk; k<=ek; k++) 
+#pragma omp parallel for
 		for (l=sl; l<=el; l++)
 			count+=board[k][l];
-	count-=board[i][j];	
+	                count-=board[i][j];
+	
 	return count;
 
 }
@@ -62,22 +64,22 @@ void play (cell_t ** board, cell_t ** newboard, int size) {
 	int	i, j, a;
 	/* for each cell, apply the rules of Life */
 
+#pragma omp parallel for private(a) collapse(2)
+           for (i=0; i<size; i++)
 
+//#pragma omp parallel for private(a)
 
-for (i=0; i<size; i++)
-#pragma omp parallel  private(a,j) 
-{
-#pragma omp for 
 		for (j=0; j<size; j++) {
-               
-			a = adjacent_to (board, size, i, j);
 
+			a = adjacent_to (board, size, i, j);
+               
 			if (a == 2) newboard[i][j] = board[i][j];
 			if (a == 3) newboard[i][j] = 1;
 			if (a < 2) newboard[i][j] = 0;
 			if (a > 3) newboard[i][j] = 0;
+
 		}
-}
+
 }
 
 /* print the life board */
@@ -86,7 +88,7 @@ void print (cell_t ** board, int size) {
 	/* for each row */
 	for (j=0; j<size; j++) {
 		/* print each column position... */
-
+               
 		for (i=0; i<size; i++) 
 			printf ("%c", board[i][j] ? 'x' : ' ');
 		/* followed by a carriage return */
@@ -104,7 +106,7 @@ void read_file (FILE * f, cell_t ** board, int size) {
 		fgets (s, size+10,f);
 		/* copy the string to the life board */
 
-#pragma omp parallel for
+#pragma omp for
 		for (i=0; i<size; i++)
 		{
 		 	//c=fgetc(f);
